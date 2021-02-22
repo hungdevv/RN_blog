e82ca4eb19b340a42b31ace6b0e7f09cc23fbba7
nguyenhung <hunghung0309@gmail.com>
2021-02-22 16:55:54 +0700

    done

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer} from 'react-navigation';
-import IndexScreen from './src/screen/IndexScreen';
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
-import ShowScreen from './src/screen/ShowScreen';
-import CreateScreen from './src/screen/CreateScreen';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
 
 const navigator = createStackNavigator(
-{
-  Index: IndexScreen,
-  Show: ShowScreen,
-  Create: CreateScreen,
-},
-{
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
+  {
+    Index: IndexScreen,
+    Show: ShowScreen,
+    Create: CreateScreen,
+    Edit: EditScreen,
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs',
+    },
   }
-}
 );
 
 const App = createAppContainer(navigator);
 
 export default () => {
-  return(
-  <Provider>
+  return (
+    <Provider>
       <App />
-  </Provider>
-  )
+    </Provider>
+  );
 };
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,34 +2,53 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blog_post':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blog_post':
-      return state.filter(blogPost => blogPost.id !== action.payload);
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blog_post':
       return [
         ...state,
         {
-          id: Math.floor(Math.random() * 99999),
-          title: `Blog Post #${state.length + 1}`
-        }
+          id: Math.floor(Math.random() * 99999999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
       ];
     default:
       return state;
   }
 };
 
-const addBlogPost = dispatch => {
-  return () => {
-    dispatch({ type: 'add_blog_post' });
+const addBlogPost = (dispatch) => {
+  return (title, content, callback) => {
+    dispatch({ type: 'add_blog_post', payload: { title, content } });
+    if (callback) {
+      callback();
+    }
   };
 };
-const deleteBlogPost = dispatch => {
-  return id => {
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
     dispatch({ type: 'delete_blog_post', payload: id });
+  };
+};
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: 'edit_blog_post',
+      payload: { id, title, content },
+    });
+    if (callback) {
+      callback();
+    }
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  []
-);  
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+);
